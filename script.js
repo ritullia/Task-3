11d11d306c37d04da108f150bdeac8550f70f80c
Rita <94860684+ritullia@users.noreply.github.com>
2023-02-15 21:33:14 +0200

    Dev (#1)
    * fetch & users function commit
    
    * users card style commit
    
    * button & fetch commit
    
    * functions & style commit
    
    * message commit
    
    * card style commit

--- script.js
+++ script.js
@@ -11,3 +11,48 @@ Pastaba: Informacija apie user'į (jo kortelė) turi turėti bent minimalų stil
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = "https://api.github.com/users";
+
+let data
+
+function getUsersApi() {
+    fetch(ENDPOINT)
+        .then((response) => response.json())
+        .then((result) => {
+            console.log(result);
+            data = getUser(result);
+        })
+        .catch((error) => console.error(error));
+
+    let message = document.getElementById('message')
+    if (message.style.display === 'none') {
+        message.style.display = "block"
+    } else {
+        message.style.display = "none"
+    };
+};
+
+function getUser(usersArr) {
+    let outputDiv = document.getElementById('output');
+
+    usersArr.forEach((data) => {
+
+        let userCard = document.createElement("div");
+        userCard.classList.add("user-card");
+
+        let login = document.createElement('p');
+        login.classList.add("user-card-text");
+        login.textContent = data.login;
+
+        let avatarUrl = document.createElement('p');
+        avatarUrl.classList.add("user-card-text");
+        avatarUrl.textContent = data.avatar_url;
+
+
+        userCard.append(login, avatarUrl);
+        outputDiv.append(userCard);
+
+    });
+
+};
+
+document.getElementById('btn').addEventListener("click", getUsersApi);
