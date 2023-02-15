edc031deb72882cb9fa59a69bef36c0c3079fd1b
ritullia <brigrita@gmail.com>
2023-02-15 20:36:34 +0200

    fetch & users function commit

--- script.js
+++ script.js
@@ -11,3 +11,41 @@ Pastaba: Informacija apie user'į (jo kortelė) turi turėti bent minimalų stil
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = "https://api.github.com/users";
+
+let data
+
+fetch(ENDPOINT)
+    .then((response) => response.json())
+    .then((result) => {
+        console.log(result);
+        data = result;
+        getUser(result);
+    })
+    .catch((error) => console.error(error));
+
+function getUser(usersArr) {
+    let outputDiv = document.getElementById('output');
+
+    usersArr.forEach((data) => {
+
+        let userCard = document.createElement("div");
+        userCard.classList.add("user-card");
+
+        let login = document.createElement('p');
+        login.textContent = data.login;
+
+        let avatarUrl = document.createElement('p');
+        avatarUrl.textContent = data.avatar_url;
+
+
+        userCard.append(login, avatarUrl)
+        outputDiv.append(userCard)
+
+
+    });
+
+
+
+}
+
+document.getElementById('btn').addEventListener("click", getUser)
